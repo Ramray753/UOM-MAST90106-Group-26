6a66dd538ff9e49f42028e8881d737476abc5cff
Ramray753 <zhaxiangtian@gmail.com>
2021-08-27 12:17:40 +1000

    add multinomial modelling code

---
+++ Jupyter/Modelling-Multi/copy.sh
@@ -0,0 +1,6 @@
+pre=MOG
+cp 'CNN - Modelling (Multi + Origin + Embedded TL).ipynb' "CNN - Modelling (Multi + ${pre} + Embedded TL).ipynb" 
+cp 'CNN - Modelling (Multi + Origin + Inception).ipynb' "CNN - Modelling (Multi + ${pre} + Inception).ipynb"
+cp 'CNN - Modelling (Multi + Origin + ResNet).ipynb' "CNN - Modelling (Multi + ${pre} + ResNet).ipynb"
+cp 'CNN - Modelling (Multi + Origin + Standalone TL).ipynb' "CNN - Modelling (Multi + ${pre} + Standalone TL).ipynb"
+cp 'CNN - Modelling (Multi + Origin + VGG).ipynb' "CNN - Modelling (Multi + ${pre} + VGG).ipynb"
